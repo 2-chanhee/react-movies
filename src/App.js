64aca26189b20457d38f5b98212ece33150e3806
2-chanhee <l01083582454@gmail.com>
2022-06-23 23:16:00 +0900

    create first component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+
+import Potato from "./potato";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// react application은 하나의 component만 rendering할 수 있음.
 root.render(
   <React.StrictMode>
     <App />

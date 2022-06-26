cb7b5e2373ac3b20b39e99091ec2c252e6086fe3
2-chanhee <54311184+2-chanhee@users.noreply.github.com>
2022-06-26 16:02:44 +0900

    first movie component (#3)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-import Potato from "./potato";
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Potato />
-    </div>
-  );
+  // component가 mount되면 바로 실행
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
